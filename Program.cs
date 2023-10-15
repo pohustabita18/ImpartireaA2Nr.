e75fea4fafa76f6a67373610a19dd8d308e90a5e
TabitaPohusUeo <147128333+pohustabita18@users.noreply.github.com>
2023-10-15 12:00:01 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InmultireA2Nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            float p;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            p = a / b;
+            if (Convert.ToInt32(p % 2 = 0))
+            {
+                Console.WriteLine("Fara rest");
+            }
+                else
+            {
+                Console.WriteLine("Cu rest");
+            }
+        }
+    }
+}
